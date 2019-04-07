3fc4924d04bbb0ec931a02d5646bb8893f0d13d5
lenovo <lenovo@DELL>
2019-04-07 13:42:28 +0530

    h

--- Sample-Projects/src/main/java/main/SampleProject.java
+++ Sample-Projects/src/main/java/main/SampleProject.java
@@ -1,18 +1,25 @@
 package main;
 
-public class SampleProject {
-	//print  Hello world! in the console
-	public static String printHello() {
-		//print Hello world! in the console!
-		String c = "Hello World!";
-		return c;
-	}
-	public static int add(int a,int b) {
-		int c = a+b;
-		return c;
-	}
-	public static void main(String args[]) {
+public class SampleProject
+{
+	public static void main(String args[]) 
+	{
 		System.out.println(printHello());
-		System.out.println(add(23,45));
+		System.out.println(add(3,46));
 	}
+	//print  Hello world! in the console
+	public static String printHello() 
+	{
+		//print Hello world! in the console!
+		String c = "";
+		c = "Hello World!";
+		return c;
+	}
+	public static int add(int a,int b) 
+	{
+		int c = 0;
+		c = a+b;
+		return c;
+	}
+
 }
